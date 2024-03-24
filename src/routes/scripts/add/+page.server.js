1d5140efbdac998b9cae0f16038e36717e6b5d30
Marco D'Agostini <madacol10@gmail.com>
2024-03-24 01:19:34 +0100

    moved validation to a function to improve endpoint readability

--- src/routes/scripts/add/+page.server.js
+++ src/routes/scripts/add/+page.server.js
@@ -10,52 +10,10 @@ export const actions = {
         const source_url = data.get("source_url");
         const user_id = locals.user?.user_id;
 
-        if (!source_url) return fail(400, {error: "You must provide a URL"})
         if (!user_id) return fail(401, {error: "You must be logged in to add a script"});
 
-        // Validate URL is http or dataURL
-        if (source_url.startsWith("data:")) {
-            // Validate if it is javascript and parses correctly
-            try {
-                const response = await fetch(source_url);
-                const text = await response.text();
-                if (text.length > 10000) {
-                    return fail(400, {error: "DataURL is too large"});
-                }
-                new Function(text);
-            } catch (e) {
-                return fail(400, {error: "DataURL is not valid JavaScript"});
-            }
-
-        } else if (source_url.match(/^https?:\/\//)) {
-
-            // Validate if server response is valid
-            try {
-                const response = await fetch(source_url)
-                if (!response.ok) {
-                    return fail(400, {error: "URL's server did not respond with 200 OK"});
-                }
-
-                // Validate allow origin header
-                const allowOrigin = response.headers.get("access-control-allow-origin");
-                if (!allowOrigin || allowOrigin !== "*") {
-                    return fail(400, {error: "URL's server does not allow cross-origin requests"});
-                }
-            } catch (e) {
-                return fail(400, {error: "failed to fetch URL"});
-            }
-
-            // validate if it parses correctly
-            try {
-                const response = await fetch(source_url);
-                const text = await response.text();
-                new Function(text);
-            } catch (e) {
-                return fail(400, {error: "URL is not valid JavaScript"});
-            }
-        } else {
-            return fail(400, {error: "URL must be HTTP or DataURL"})
-        }
+        const failed = await isURLInvalid(source_url);
+        if (failed) return failed;
 
         try {
             const {rows: [new_script]} = await sql`
@@ -71,6 +29,60 @@ export const actions = {
                 return fail(400, {error: "URL already exists in database"});
             }
             throw error;
+        }
+
+        /**
+         * @param {string} url 
+         */
+        async function isURLInvalid(url) {
+
+            if (!url) return fail(400, {error: "You must provide a URL"})
+
+            // Validate URL is http or dataURL
+            if (url.startsWith("data:")) {
+                // Validate if it is javascript and parses correctly
+                try {
+                    const response = await fetch(url);
+                    const text = await response.text();
+                    if (text.length > 10000) {
+                        return fail(400, {error: "DataURL is too large"});
+                    }
+                    new Function(text);
+                } catch (e) {
+                    return fail(400, {error: "DataURL is not valid JavaScript"});
+                }
+
+            } else if (url.match(/^https?:\/\//)) {
+
+            // Validate if server response is valid
+                try {
+                    const response = await fetch(url)
+                    if (!response.ok) {
+                        return fail(400, {error: "URL's server did not respond with 200 OK"});
+                    }
+
+                    // Validate allow origin header
+                    const allowOrigin = response.headers.get("access-control-allow-origin");
+                    if (!allowOrigin || allowOrigin !== "*") {
+                        return fail(400, {error: "URL's server does not allow cross-origin requests"});
+                    }
+                } catch (e) {
+                    return fail(400, {error: "failed to fetch URL"});
+                }
+
+                // validate if it parses correctly
+                try {
+                    const response = await fetch(url);
+                    const text = await response.text();
+                    new Function(text);
+                } catch (e) {
+                    return fail(400, {error: "URL is not valid JavaScript"});
+                }
+            } else {
+                return fail(400, {error: "URL must be HTTP or DataURL"})
+            }
+
+            return false;
         }
     },
 }
