8b053861ff1fc745d0820cfca094a591a8053681
Marco D'Agostini <madacol10@gmail.com>
2024-04-10 14:04:42 +0200

    Fix: Oops, non-admins could see logs

--- src/routes/logs/+page.server.js
+++ src/routes/logs/+page.server.js
@@ -1,29 +1,33 @@
+import { checkPermissions_MW } from "$lib/server/checkPermissions_MW";
 import { sql } from "$lib/server/db";
 
-export async function load() {
+export const load = checkPermissions_MW(
+    'read_users',
+    async () => {
 
-    const {rows: logs} = await sql`
-        SELECT
-            path,
-            user_session->'username' as username,
-            created_at,
-            method,
-            response_status as status,
-            headers->'referer' as referer,
-            headers->'x-vercel-ip-city' as city,
-            headers->'x-vercel-ip-timezone' as timezone,
-            headers->'x-vercel-ip-country' as country,
-            headers->'x-real-ip' as ip,
-            headers->'user-agent' as user_agent,
-            headers,
-            user_session,
-            params,
-            body
-        FROM logs
-        ORDER BY created_at DESC
-        LIMIT 500;
-    `
-    return {
-        logs
+        const {rows: logs} =await sql`
+            SELECT
+                path,
+                user_session->'username' as username,
+                created_at,
+                method,
+                response_status as status,
+                headers->'referer' as referer,
+                headers->'x-vercel-ip-city' as city,
+                headers->'x-vercel-ip-timezone' as timezone,
+                headers->'x-vercel-ip-country' as country,
+                headers->'x-real-ip' as ip,
+                headers->'user-agent' as user_agent,
+                headers,
+                user_session,
+                params,
+                body
+            FROM logs
+            ORDER BY created_at DESC
+            LIMIT 500;
+        `
+        return {
+            logs
+        }
     }
-}
+)
---
+++ tests/logged in/other.test.js
@@ -0,0 +1,9 @@
+import test, { expect } from "@playwright/test";
+
+test('non-admin cannot access logs', async ({ page }) => {
+    // Try to access the `/logs` route directly
+    const response = await page.goto('/logs');
+
+    // Verify response status code is 403
+    expect(response?.status()).toBe(403);
+});
