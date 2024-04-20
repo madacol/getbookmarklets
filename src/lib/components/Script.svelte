530b9f06147a9ce8ef451bd36549c4c1530e2b6c
Marco D'Agostini <madacol10@gmail.com>
2024-04-20 13:16:01 +0200

    shown signal was executing on each key stroke when editing

--- src/lib/components/Script.svelte
+++ src/lib/components/Script.svelte
@@ -15,7 +15,7 @@
 
     let showCode = $state(!collapseCode);
 
-    $effect(() => { showCode && fetch(`/logs/shown/${encodeURIComponent(source_url)}`, { method: 'POST' }) });
+    $effect(() => { showCode && fetch(`/logs/shown/${encodeURIComponent(untrack(()=>source_url))}`, { method: 'POST' }) });
 
     $effect(() => {
         if (!source_url || (isDataURL && source)) return;
