46a4387a0aa5097bedcd4ea515c3ab847eaadd86
Marco D'Agostini <madacol10@gmail.com>
2024-05-13 10:01:46 +0200

    Improve introduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,32 +14,31 @@
 
         <h2>How this site works</h2>
 
-        <p>The server stores a bookmarklet as a single URL, everything else is derived from it.</p>
-
-        <h3>Metadata</h3>
-        <p>Located in the code in the first lines of comments (Like userscripts), and is parsed in the client</p>
-        <p>Currently only 4 tags are supported:</p>
+        <p>Scripts are stored as URLs.</p>
+        <p>To add a script</p>
+        <ol>
+            <li>Navigate to <a href="/scripts/add">Add script</a>.</li>
+            <li>Paste the URL of a RAW JavaScript file.</li>
+        </ol>
+        <p>You can add metadata using userscript-like comments. Supported tags:</p>
         <ul>
-            <li><code>// @name</code> - if not present, defaults to filename.</li>
+            <li><code>// @name</code> – if not present, defaults to the URL's filename.</li>
             <li><code>// @description</code></li>
-            <li><code>// @image</code> - URL to an image.</li>
-            <li><code>// @video</code> - URL to a video.</li>
+            <li><code>// @image</code> – URL to an image that will be displayed on this site. You can add multiple images.</li>
+            <li><code>// @video</code> – URL to a video. You can add many.</li>
         </ul>
-        <p>Images and Videos are not rendered until you click on the "Show Media" button.</p>
 
-        <h3>Who can upload?</h3>
-        <p>Any IP, once per day.</p>
 
 
         <h2>Why? (or Rant)</h2>
         <p>Honestly, bookmarklets and userscripts should not exist. The fact that they still do is a failure of the web extension ecosystem.</p>
         <p>
-            Extensions are difficult to verify that they <strong>don't do anything else</strong> than what they claim.
+            Extensions are difficult to verify that they <i>don't do anything else</i> than what they claim.
             <br>The permission system is too broad, and the time and knowledge you need to get to the source code is too high. Making it look like a joke how much harder it is to verify compared to an equivalent one-liner bookmarklet.
         </p>
         <p>Since the minimum effort needed to verify is really high, and average users cannot verify it anyway, browsers decided to focus on methods that end up putting friction/restrictions to what you can do with your browser.</p>
-        <p>Narrowing the permission system would help <strong>a lot</strong>, but it requires lots of efforts into coordination and implementation. So it only makes sense for the most popular restrictions users want. The long tail will still be neglected.</p>
-        <p>So I think browsers should also focus on making it easier to verify extension's code for developers, and let social signaling do the rest (e.g. developers --influence-> power users --influence-> average users).</p>
+        <p>Narrowing the permission system would help <i>a lot</i>, but it requires lots of efforts into coordination and implementation. So it only makes sense for the most popular restrictions users want. The long tail will still be neglected.</p>
+        <p>So I think extension platforms should make it easier to verify extension's code, and let social signaling do the rest (e.g. developers --influence-> power users --influence-> average users).</p>
 
 
         <h2>What are Bookmarklets?</h2>
