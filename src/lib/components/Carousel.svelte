992a85e1def2b4e083c60f85e167b985ca6b167e
Marco D'Agostini <madacol10@gmail.com>
2024-09-23 14:40:11 +0200

    fix: when multiple instances, all expanded at the same time

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,77 +1,73 @@
 <script>
-    import { pushState, replaceState } from "$app/navigation";
-    import { page } from "$app/stores";
-
     /**
      * @type {{ medias: { key: string, value: string }[] }}
      */
     let { medias } = $props();
 
-    /**
-     * @type {HTMLElement | null}
-     */
+    let isExpanded = $state(false);
+
+    /** @type {HTMLElement | null} */
     let expandedElement = $state(null);
-    replaceState('', { expanded: false });
 
     $effect(() => {
-        if ($page.state?.expanded) {
-            expandedElement?.scrollIntoView({inline: 'center', block: 'center'})
+        const element = expandedElement;
+        if (element) {
+            setTimeout(() => {
+                element?.scrollIntoView({inline: 'center', block: 'center'})
+            });
         }
     })
 
-    function toggleExpanded(e) {
-        if ($page.state?.expanded) {
-            expandedElement = null
-            history.back();
-        } else {
-            expandedElement = e.target;
-            pushState('', { expanded: true });
-        }
+    /** @param {MouseEvent} event */
+    function toggleExpanded(event) {
+        isExpanded = !isExpanded;
+        expandedElement = event.target instanceof HTMLElement ? event.target : null;
     }
 </script>
 
-<div class="carousel" class:expanded={$page.state?.expanded} aria-expanded={$page.state?.expanded}
+<div class="carousel"
     onclick={toggleExpanded}
     onkeydown={(e) => {
-        if (e.key === 'Escape' && $page.state?.expanded) toggleExpanded(e);
-        else if (e.key === 'Enter') toggleExpanded(e)
+        if (e.key === 'Escape' && isExpanded) toggleExpanded(e);
+        else if (e.key === 'Enter') toggleExpanded(e);
     }}
     role="button"
     tabindex="0"
 >
-    {#each medias as {key, value}}
-        {#if key === 'image'}
-            <img src={value} loading="lazy" />
-        {:else if key === 'video'}
-            <video src={value} controls preload="metadata"></video>
-        {/if}
-    {/each}
+    <div class="carousel-popover" aria-expanded={isExpanded} popover={isExpanded ? "manual" : null}>
+        {#each medias as {key, value}}
+            {#if key === 'image'}
+                <img src={value} loading="lazy" />
+            {:else if key === 'video'}
+                <video src={value} controls preload="metadata"></video>
+            {/if}
+        {/each}
+    </div>
 </div>
 
 <style>
     .carousel {
-        display: flex;
-        gap: 1rem;
-        padding: 1rem 0;
-        max-width: 100%;
-        overflow: auto;
         height: 15rem;
-        cursor: pointer;
-        z-index: 1;
 
-        &:hover:not(.expanded) {
-            opacity: 0.9;
+        .carousel-popover {
+            display: flex;
+            gap: 1rem;
+            padding: 1rem 0;
+            max-width: 100%;
+            height: 100%;
+            box-sizing: border-box;
+            overflow: auto;
+            cursor: pointer;
+            z-index: 1;
         }
-        &.expanded {
-            position: fixed;
+
+        .carousel-popover[aria-expanded="true"] {
             height: 100vh;
-            top: 0;
-            left: 0;
             padding: 5vh 0;
             gap: 5vh;
-            box-sizing: border-box;
             background: rgba(0, 0, 0, 0.7);
         }
+
         img, video {
             height: 100%;
             max-width: 100vw;
@@ -79,4 +75,5 @@
             object-fit: contain;
         }
     }
+
 </style>
